4e57dba3325dbf53b5a9059aa351e417cb7c9357
Mordyg <91845645+Mordles@users.noreply.github.com>
2021-10-26 21:14:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import smbus
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+
+
+import PCF8591 as ADC
+
+def setup():
+  ADC.setup(0x48)
+
+def loop():
+  while True:
+    print(ADC.read(0))
+    ADC.write(ADC.read(0))
+
+def destroy():
+  ADC.write(0)
+
+if __name__ == "__main__":
+  try:
+    setup()
+    loop()
+  except KeyboardInterrupt:
+    destroy()
+
+
